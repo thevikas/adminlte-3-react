5d16852a4feb4aec2d8481bde892e3cccda180e5
Vikas Yadav <mevikas@gmail.com>
2021-03-31 09:17:47 +0530

    added small search box

--- src/components/ReactTable.js
+++ src/components/ReactTable.js
@@ -1,48 +1,66 @@
 // Table.js
 
-import React from "react";
-import { useTable } from "react-table";
+import React, { useState } from "react";
+import { useTable, useFilters, useSortBy } from "react-table";
 
 export default function ReactTable({ columns, data }) {
+
+  const [filterInput, setFilterInput] = useState("");
+
+  const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    setFilter("col2", value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+    setFilterInput(value);
+  };
+
   // Use the useTable Hook to send the columns and data to build the table
   const {
     getTableProps, // table props from react-table
     getTableBodyProps, // table body props from react-table
     headerGroups, // headerGroups, if your table has groupings
     rows, // rows for the table based on the data passed
-    prepareRow // Prepare the row (this function needs to be called for each row before getting the row props)
+    prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
+    setFilter
   } = useTable({
     columns,
     data
-  });
+  },
+    useFilters);
 
   /*
     Render the UI for your table
     - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
   */
   return (
-    <table {...getTableProps()} class="table table-bordered table-striped">
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+    <>
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search name"}
+      />
+      <table {...getTableProps()} class="table table-bordered table-striped">
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
